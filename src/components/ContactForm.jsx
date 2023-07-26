35868d44e0cd12bf475cd2f57468ff63da177803
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2023-07-26 23:06:19 +0200

    ContactForm reset fixed

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,33 +1,81 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import SocialNetwork from './SocialNetwork';
 import emailjs from '@emailjs/browser';
 
-
 const ContactForm = () => {
-
     const form = useRef();
 
-    const handleSubmit = (e)=>{
-        e.preventDefault()
-        
-        emailjs.sendForm('service_5wk0o1s', 'template_5agglvh', form.current, 'WcTjx_hDSVUI77y2w')
-        .then((result) => {
-            console.log(result.text);
-            alert('Soon, I\'ll read your message! Thank You! 😊')
-        }, (error) => {
-            console.log(error.text);
-        });
-    }
+    const [firstname, setFirstname] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [textarea, setTextarea] = useState('');
+
+    console.log(lastName, firstname, email, textarea);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        emailjs
+            .sendForm(
+                'service_5wk0o1s',
+                'template_5agglvh',
+                form.current,
+                'WcTjx_hDSVUI77y2w'
+            )
+            .then(
+                (result) => {
+                    console.log(result.text);
+                    setEmail('');
+                    setLastName('');
+                    setFirstname('');
+                    setEmail('');
+                    setTextarea('');
+                    alert("Soon, I'll read your message! Thank You! 😊");
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
+    };
 
     return (
         <div className='contact-form'>
             <form ref={form} onSubmit={handleSubmit}>
-                <input type="text" name='lastname' placeholder='Lastname' required />
-                <input type="text" name='firstname' placeholder='Firstname'required />
-                <input type="email" name='email' placeholder='Email' required />
-                <textarea name="message" rows="9" cols="70" maxLength="500" minLength="10" required placeholder="Write your message here"></textarea>
+                <input
+                    type='text'
+                    name='lastname'
+                    placeholder='Lastname'
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                    required
+                />
+                <input
+                    type='text'
+                    name='firstname'
+                    placeholder='Firstname'
+                    value={firstname}
+                    onChange={(e) => setFirstname(e.target.value)}
+                    required
+                />
+                <input
+                    type='email'
+                    name='email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder='Email'
+                    required
+                />
+                <textarea
+                    name='message'
+                    rows='9'
+                    cols='70'
+                    maxLength='500'
+                    minLength='10'
+                    required
+                    value={textarea}
+                    onChange={(e) => setTextarea(e.target.value)}
+                    placeholder='Write your message here'></textarea>
 
-                <button type="submit">Send</button>
+                <button type='submit'>Send</button>
             </form>
 
             <SocialNetwork />
